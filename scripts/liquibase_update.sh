ffee2b2a51d24d42579e5a27d1524c61b1c7b523
John Boyes <john@agilepathway.co.uk>
2022-10-10 14:51:23 +0000

    Run Liquibase in headless mode
    We run it in headless mode because we are invoking it from a script
    which gets automatically invoked by the devcontainer as a [postcreate
    command][1].
    
    [1]: https://containers.dev/implementors/json_reference/#lifecycle-scripts

--- scripts/liquibase_update.sh
+++ scripts/liquibase_update.sh
@@ -4,6 +4,7 @@ liquibase \
   --classpath="$LIQUIBASE_CLASSPATH" \
   --hub-mode="$LIQUIBASE_HUB_MODE" \
   --strict="$LIQUIBASE_STRICT" \
+  --headless="$LIQUIBASE_HEADLESS" \
   update \
   --changelog-file="$LIQUIBASE_CHANGELOG_FILE" \
   --url="$LIQUIBASE_COMMAND_URL" \
