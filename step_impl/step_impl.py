ec33eb1f499cc78c197f3319b706e2b57b63ffc4
John Boyes <john@agilepathway.co.uk>
2022-10-11 16:56:17 +0300

    Add transactions via gauge (#2)
    * Install Gauge on devcontainer
    
    * Install screenshot Gauge plugin on devcontainer
    
    Even though we don't use screenshots (and even though we have now
    configured Gauge to not try to take screenshots), Gauge still installs
    the screenshot plugin if its not there when running Gauge specs for the
    first time.  So we now just install the plugin on the devcontainer to
    save that overhead when running the specs.
    
    * Add default Gauge python example spec
    
    These files were generated by running `gauge init python`, which
    generates an example Gauge spec and step implementation, along with the
    other scaffolding files needed.
    
    * Turn off gauge screenshots
    
    * Add transactions Gauge spec
    
    Also remove the example Gauge spec.
    
    * Use Black to format Python code
    
    [Black][1] is an opinionated Python code formatter (its name comes from
    the saying "any colour you like, as long as it's black").
    
    This commit has the devcontainer automatically format Python files with
    Black upon file save.
    
    As [this blog article about Golang's code formatter][2] (which is
    similar in intent to Black) explains, benefits of an opinionated code
    formatter include:
    
    -  easier to write: never worry about minor formatting concerns while
       hacking away
    
    -  easier to read: when all code looks the same you need not mentally
       convert others' formatting style into something you can understand
    
    -  easier to maintain: mechanical changes to the source don't cause
       unrelated changes to the file's formatting; diffs show only the real
       changes.
    
    -  uncontroversial: never have a debate about spacing or brace position
       ever again!
    
    [1]: https://black.readthedocs.io
    [2]: https://go.dev/blog/gofmt

---
+++ step_impl/step_impl.py
@@ -0,0 +1,44 @@
+import os
+import random
+import time
+from getgauge.python import step
+import psycopg2
+
+
+def end_time(duration):
+    return time.time() + int(duration)
+
+
+def connection():
+    return psycopg2.connect(
+        host=os.environ.get("PGHOST"),
+        database=os.environ.get("DATABASE_NAME"),
+        user=os.environ.get("LIQUIBASE_COMMAND_USERNAME"),
+        password=os.environ.get("LIQUIBASE_COMMAND_PASSWORD"),
+    )
+
+
+def random_amount():
+    return random.randint(1, 100000) / 100
+
+
+@step("Add a transaction every <frequency> seconds for <duration> seconds")
+def add_transactions(frequency, duration):
+    print("Adding transactions . . ")
+    sql = """INSERT INTO transactions(amount, time)
+             VALUES(%s, now()) RETURNING id;"""
+    conn = None
+    try:
+        conn = connection()
+        cur = conn.cursor()
+        t_end = end_time(duration)
+        while time.time() < t_end:
+            cur.execute(sql, (random_amount(),))
+            time.sleep(int(frequency))
+        conn.commit()
+        cur.close()
+    except (psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
