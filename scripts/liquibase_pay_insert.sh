ab4233d9f2c3593d395bb686f1fc1aa6aa9a8ab3
cfagaras <69666348+cfagaras@users.noreply.github.com>
2022-12-12 19:46:41 +0000

    commit1

--- scripts/liquibase_pay_insert.sh
+++ scripts/liquibase_pay_insert.sh
@@ -11,3 +11,4 @@ liquibase \
   --url="jdbc:postgresql://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME" \
   --username="$DATABASE_USERNAME" \
   --password="$DATABASE_PASSWORD" \
+  --log-level=FINE \
