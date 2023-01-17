0ca92c8cb6ea3666a4c50184cf3c38afcb94ab5c
cfagaras <fagaras.cristian@gmail.com>
2023-01-17 10:07:08 +0000

    branch creation

---
+++ scripts/liquibase_pay_update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+liquibase \
+  --classpath="$LIQUIBASE_CLASSPATH" \
+  --hub-mode="$LIQUIBASE_HUB_MODE" \
+  --strict="$LIQUIBASE_STRICT" \
+  --headless="$LIQUIBASE_HEADLESS" \
+  update \
+  --changelog-file="$LIQUIBASE_CHANGELOG_FILE" \
+  --contexts="load-data-update" \
+  --url="jdbc:postgresql://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME" \
+  --username="$DATABASE_USERNAME" \
+  --password="$DATABASE_PASSWORD" \
